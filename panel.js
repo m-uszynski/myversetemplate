5f64242826736c69e0a82ef8fb6c9e4ed32829b2
Mateusz Uszyński <mtb.uszynski@gmail.com>
2019-08-27 19:36:57 +0200

    Initial commit

---
+++ panel.js
@@ -0,0 +1,10 @@
+function showPanel(){
+    var panel = document.getElementById("user-panel");
+    panel.classList.toggle("show");
+}
+
+/*document.addEventListener('mouseup', function(event){
+    var panel = document.getElementById("user-panel");
+    if(panel.classList.contains("show")) panel.classList.remove("show");
+});*/
+
